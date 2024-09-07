7bc0fb09140f82fca8477ecda2a97be2d7789a92
ChhoeurnKhuon <Khounchhoeun66@gmail.com>
2024-09-08 01:22:29 +0700

    add more service

--- src/main/java/co/istad/springwebmvc/service/ProductService.java
+++ src/main/java/co/istad/springwebmvc/service/ProductService.java
@@ -1,1 +1,1 @@
-package co.istad.springwebmvc.service;import co.istad.springwebmvc.dto.ProductDto;import java.util.List;public interface ProductService {    List<ProductDto> findProducts();//    ProductDto findProductById();}
+package co.istad.springwebmvc.service;import co.istad.springwebmvc.dto.ProductDto;import java.util.List;public interface ProductService {    List<ProductDto> findProducts(String name, Boolean s);    ProductDto findProductById(Integer id);    ProductDto findProductByUuid(String uuid);}
